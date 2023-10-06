c42244c8377ca476992549473fa4990512e5210c
Duh0127 <rodatupmocgamer22@gmail.com>
2023-10-05 23:30:50 -0300

    inicio e mostrando rotas inclusas do NEXT

---
+++ src/app/produtos/page.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function page(){
+    return(
+        <>
+            <h1>PRODUTOS</h1>
+            <p>Página Inicial - <Link href="/">VOLTAR</Link></p>
+        </>
+    )
+}
